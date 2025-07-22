2dd7ab1185ebc6d70ebac1beece0547a2a4f97fb
Jedlord <jacobysmonkey@gmail.com>
2025-07-21 20:14:07 -0700

    Test

---
+++ MaoMao/View/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MaoMao
+{
+    public partial class MainPage : ContentPage
+    {
+        private int timesClicked = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+		void OnButtonClicked(object sender, EventArgs e)
+		{
+			timesClicked++;
+			label.Text = $"Clicked {timesClicked} times!";
+		}
+	}
+}
