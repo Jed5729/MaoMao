566370848b595ddd5be0ad8b0db45ccfe04a17f2
Jedlord <jacobysmonkey@gmail.com>
2026-03-12 21:14:10 -0700

    Add project files.

---
+++ MaoMao/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MaoMao.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MaoMao/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MaoMao.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
